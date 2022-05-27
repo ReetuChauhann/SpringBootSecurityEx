c6869de8203f805d3c7ac16a0eca353884a094a6
ReetuChauhann <chauhanreetu00@gmail.com>
2022-05-27 17:06:30 +0530

    SpringBoot Security Example

---
+++ src/test/java/com/reetu/SpringBootSecurityExApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reetu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSecurityExApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
